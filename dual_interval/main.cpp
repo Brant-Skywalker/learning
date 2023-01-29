503d8f545330e4a6ede2d1c4daf4e86514948a78
brant-skywalker <1773159722@qq.com>
2023-01-28 23:59:40 -0600

    Added driver.

---
+++ dual_interval/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "dual.h"
+
+int main() {
+    using std::sin;
+    using std::cos;
+    using std::exp;
+    Dual d1{3.7, 5.7};
+    Dual d2{2.3, 5.8};
+
+    std::cout << d1 + d2 << std::endl;
+    std::cout << d1 - d2 << std::endl;
+    std::cout << d1 * d2 << std::endl;
+    std::cout << sin(d1) << std::endl;
+    std::cout << exp(d1) << std::endl;
+
+    return 0;
+}
